0b3b39c713be7199e35d38e7d3cc42330b7c688f
asmraihan <asmraihanctg14@gmail.com>
2023-03-27 14:41:13 +0600

    modal fetched

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import Button from '../Button/Button';
+import Modal from '../Modal/Modal';
 import SingleData from '../SingleData/SingleData';
 
 const Card = () => {
 const [data, setData] = useState([])
+const [singleData, setSingleData] =useState({})
 const [showAll, setShowAll] = useState(false)
+const [uniqueId, setUniqueId] = useState(null)
 
+// console.log(singleData)
 // const handleShowAll =() =>{ /* for nubs */
 //     setShowAll(true)
 // } 
+useEffect(()=>{
+    // console.log('hlw frm useEfct')
+    fetch(`https://openapi.programming-hero.com/api/ai/tool/${uniqueId}`)
+    .then(res => res.json())
+    .then(data => setSingleData(data.data))
+},[uniqueId])
 
 useEffect(()=>{
     const loadData= async() =>{
@@ -24,16 +34,22 @@ useEffect(()=>{
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 lg:px-12 w-11/12 mx-auto'>
             {
                 data.slice(0, showAll ? 12 : 6).map((singleData) =>(
-             <SingleData singleData={singleData} key={singleData.id}></SingleData>
+             <SingleData 
+                singleData={singleData} 
+                key={singleData.id}
+                setUniqueId={setUniqueId}> {/* singledatar kase set unique id pass korlam */}
+    
+                </SingleData>
 
             ))}
         </div>
     {   
-    !showAll&& <span
+    !showAll&& (<span
     onClick={()=>setShowAll(true)}>
     <Button>Show More</Button>
-    </span>
-    }       
+    </span>)
+    }
+    <Modal singleData={singleData} />   
         </>
 
     );
--- src/component/SingleData/SingleData.jsx
+++ src/component/SingleData/SingleData.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const SingleData = (props) => {
     // console.log(props.singleData)
-    const {image, features,published_in,name } = props.singleData
+    const {id, image, features,published_in,name, } = props.singleData
     // console.log(features)
+ 
     return (
         <>
             <div className="card w-96 bg-base-100 shadow-2xl">
@@ -26,12 +27,11 @@ const SingleData = (props) => {
         <p>{published_in}</p>
     </div>
     <div>
-        <span className='cursor-pointer'>
+        <label onClick={()=>props.setUniqueId(id)} htmlFor="my-modal-5" className='cursor-pointer'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6  ">
   <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
 </svg>
-
-        </span>
+        </label>
     </div>
     </div>
     {/* <p>If a dog chews shoes whose shoes does he choose?</p>
